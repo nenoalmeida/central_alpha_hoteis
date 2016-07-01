cf33d3d2910b9a94724384c08ad4f1f9d0421925
NenoAlmeida <nenoxvx@icloud.com>
2016-06-30 23:00:17 -0300

    Adding CI

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :hotels
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   
 
